be486a07f0c4606dd3402166ce766b52342997d7
thomas-bressel <tbressel@outlook.fr>
2025-09-22 16:42:45 +0200

    fix: add more feeds

--- src/Lib/Constants/FeedUrls.ts
+++ src/Lib/Constants/FeedUrls.ts
@@ -15,136 +15,136 @@ export const DEFAULT_RSS_FEEDS: IRSSFeed[] = [
     fetchInterval: 60, // 1 hour
     tags: ['amstrad-cpc', 'c64', 'zx-spectrum', 'indie', 'homebrew'],
   },
-  // {
-  //   id: 'amstrad-eu',
-  //   name: 'Amstrad.eu',
-  //   url: 'https://amstrad.eu/feed/',
-  //   category: CATEGORIES.AMSTRAD_CPC,
-  //   description: 'Specialized Amstrad community and news',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['amstrad-cpc', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'octoate',
-  //   name: 'Octoate.de',
-  //   url: 'https://www.octoate.de/feed/',
-  //   category: CATEGORIES.AMSTRAD_CPC,
-  //   description: 'General news on amstrad CPC Tricks',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['amstrad-cpc', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'atariage',
-  //   name: 'Atariage',
-  //   url: 'https://www.atariage.com/news/rss.php',
-  //   category: CATEGORIES.ATARI_ST,
-  //   description: 'General news on Atari ST micro computers',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['atari-st', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'retrogamerie',
-  //   name: 'La Rétrogamerie',
-  //   url: 'https://retrogamerie.fr/feed/',
-  //   category: CATEGORIES.RETRO_GAMING,
-  //   description: 'General news on Atari ST micro computers',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['atari-st', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'itchio',
-  //   name: 'Itch.io',
-  //   url: 'https://itch.io/feed/new.xml',
-  //   category: CATEGORIES.HOMEBREW,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['homebrew', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'retro-rgb',
-  //   name: 'Retro RGB',
-  //   url: 'https://www.retrorgb.com/feed',
-  //   category: CATEGORIES.HOMEBREW,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['homebrew', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'indiedb',
-  //   name: 'Indie DB',
-  //   url: 'https://rss.indiedb.com/articles/feed/rss.xml',
-  //   category: CATEGORIES.HOMEBREW,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['homebrew', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'gameradar',
-  //   name: 'Game Radar',
-  //   url: 'https://www.gamesradar.com/feeds.xml',
-  //   category: CATEGORIES.RETRO_GAMING,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['homebrew', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'vintageisthenewold',
-  //   name: 'Vintage is the New Old',
-  //   url: 'https://www.vintageisthenewold.com/feed/',
-  //   category: CATEGORIES.RETRO_GAMING,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['homebrew', 'hardware', 'community'],
-  // },
-  // {
-  //   id: 'scene-world',
-  //   name: 'Scene World',
-  //   url: 'https://feeds.feedburner.com/sceneworldpodcast',
-  //   category: CATEGORIES.RETRO_GAMING,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['demo', 'scene', 'community'],
-  // },
-  // {
-  //   id: 'jeux-video',
-  //   name: 'Jeux Video.com',
-  //   url: 'https://www.jeuxvideo.com/rss/rss-news.xml',
-  //   category: CATEGORIES.NEXTGEN,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['demo', 'scene', 'community'],
-  // },
-  // {
-  //   id: 'abandonware',
-  //   name: 'Abandoware France',
-  //   url: 'https://www.abandonware-france.org/rss/abandonware/',
-  //   category: CATEGORIES.RETRO_GAMING,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['demo', 'scene', 'community'],
-  // },
-  // {
-  //   id: 'rom-game',
-  //   name: 'Rom Game',
-  //   url: 'https://www.rom-game.fr/rss/rss.rss',
-  //   category: CATEGORIES.RETRO_GAMING,
-  //   description: 'General news on homebrew games',
-  //   isActive: true,
-  //   fetchInterval: 60, // 1 hour
-  //   tags: ['demo', 'scene', 'community'],
-  // },
+  {
+    id: 'amstrad-eu',
+    name: 'Amstrad.eu',
+    url: 'https://amstrad.eu/feed/',
+    category: CATEGORIES.AMSTRAD_CPC,
+    description: 'Specialized Amstrad community and news',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['amstrad-cpc', 'hardware', 'community'],
+  },
+  {
+    id: 'octoate',
+    name: 'Octoate.de',
+    url: 'https://www.octoate.de/feed/',
+    category: CATEGORIES.AMSTRAD_CPC,
+    description: 'General news on amstrad CPC Tricks',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['amstrad-cpc', 'hardware', 'community'],
+  },
+  {
+    id: 'atariage',
+    name: 'Atariage',
+    url: 'https://www.atariage.com/news/rss.php',
+    category: CATEGORIES.ATARI_ST,
+    description: 'General news on Atari ST micro computers',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['atari-st', 'hardware', 'community'],
+  },
+  {
+    id: 'retrogamerie',
+    name: 'La Rétrogamerie',
+    url: 'https://retrogamerie.fr/feed/',
+    category: CATEGORIES.RETRO_GAMING,
+    description: 'General news on Atari ST micro computers',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['atari-st', 'hardware', 'community'],
+  },
+  {
+    id: 'itchio',
+    name: 'Itch.io',
+    url: 'https://itch.io/feed/new.xml',
+    category: CATEGORIES.HOMEBREW,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['homebrew', 'hardware', 'community'],
+  },
+  {
+    id: 'retro-rgb',
+    name: 'Retro RGB',
+    url: 'https://www.retrorgb.com/feed',
+    category: CATEGORIES.HOMEBREW,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['homebrew', 'hardware', 'community'],
+  },
+  {
+    id: 'indiedb',
+    name: 'Indie DB',
+    url: 'https://rss.indiedb.com/articles/feed/rss.xml',
+    category: CATEGORIES.HOMEBREW,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['homebrew', 'hardware', 'community'],
+  },
+  {
+    id: 'gameradar',
+    name: 'Game Radar',
+    url: 'https://www.gamesradar.com/feeds.xml',
+    category: CATEGORIES.RETRO_GAMING,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['homebrew', 'hardware', 'community'],
+  },
+  {
+    id: 'vintageisthenewold',
+    name: 'Vintage is the New Old',
+    url: 'https://www.vintageisthenewold.com/feed/',
+    category: CATEGORIES.RETRO_GAMING,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['homebrew', 'hardware', 'community'],
+  },
+  {
+    id: 'scene-world',
+    name: 'Scene World',
+    url: 'https://feeds.feedburner.com/sceneworldpodcast',
+    category: CATEGORIES.RETRO_GAMING,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['demo', 'scene', 'community'],
+  },
+  {
+    id: 'jeux-video',
+    name: 'Jeux Video.com',
+    url: 'https://www.jeuxvideo.com/rss/rss-news.xml',
+    category: CATEGORIES.NEXTGEN,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['demo', 'scene', 'community'],
+  },
+  {
+    id: 'abandonware',
+    name: 'Abandoware France',
+    url: 'https://www.abandonware-france.org/rss/abandonware/',
+    category: CATEGORIES.RETRO_GAMING,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['demo', 'scene', 'community'],
+  },
+  {
+    id: 'rom-game',
+    name: 'Rom Game',
+    url: 'https://www.rom-game.fr/rss/rss.rss',
+    category: CATEGORIES.RETRO_GAMING,
+    description: 'General news on homebrew games',
+    isActive: true,
+    fetchInterval: 60, // 1 hour
+    tags: ['demo', 'scene', 'community'],
+  },
   // Add more feeds as discovered
 ];
 
